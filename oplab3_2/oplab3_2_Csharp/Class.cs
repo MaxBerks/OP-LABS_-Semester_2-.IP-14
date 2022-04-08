aaf54757514d4e170a8c57f86a5e430ee047f02c
MaxBerks <89969030+MaxBerks@users.noreply.github.com>
2022-04-08 19:05:38 +0300

    Add files via upload

---
+++ oplab3_2/oplab3_2_Csharp/Class.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oplab3_Csharp
+{
+    class Vector
+    {
+        private double X;
+        private double Y;
+        private double Z;
+
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector()
+        {
+        }
+
+        public double GetX()
+        {
+            return X;
+        }
+
+        public double GetY()
+        {
+            return X;
+        }
+
+        public double GetZ()
+        {
+            return X;
+        }
+
+        public double VectorLength()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"x: {X}  y: {Y} z:{Z}");
+        }
+
+        public void PrintWithLength()
+        {
+            Console.WriteLine($"x: {X}  y: {Y} z:{Z} len:{Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2))}");
+        }
+    }
+}
---
+++ oplab3_2/oplab3_2_Csharp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace oplab3_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vectors = InitList();
+            OutPut(vectors, "Here your vectors:");
+            Console.WriteLine("\nHere your vectors with length:");
+            var longest = Longest(vectors);
+            Console.WriteLine("\nThe longest vector is:");
+            longest.Print();
+        }
+
+        private static List<Vector> InitList()
+        {
+            var vectors = new List<Vector>()
+            {
+                new Vector(1, 3, 6), //6.78...
+                new Vector(4, 6, 2), //7.48...
+                new Vector(1, 1, 4), //4.24...
+            };
+
+            return vectors;
+        }
+
+        public static Vector Longest(List<Vector> vectors)
+        {
+            Vector longest = new Vector();
+            double length = 0;
+            foreach (var vector in vectors)
+            {
+                vector.PrintWithLength();
+                if (vector.VectorLength() > length)
+                { 
+                    length = vector.VectorLength();
+                    longest = vector;
+                }
+            }
+            return longest;
+        }
+
+        public static void OutPut(List<Vector> vectors, string prompt)
+        {
+            Console.WriteLine(prompt);
+            foreach(var vector in vectors)
+            {
+                vector.Print();
+            }
+        }
+    }
+
+    
+}
